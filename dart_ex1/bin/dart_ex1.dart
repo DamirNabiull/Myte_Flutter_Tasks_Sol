13cc70fddf6af4840ec71b6a401024f25358edbb
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2021-08-20 17:59:15 +0300

    Updated solution

--- dart_ex1/bin/dart_ex1.dart
+++ dart_ex1/bin/dart_ex1.dart
@@ -1,13 +1,15 @@
 import 'dart:math';
 
 void main() {
-  var list = [];
+  var list = [], k = 0, ans = "";
   for (var i = 0; i < 10; i++) {
     list.add(Random().nextInt(1000));
   }
   list.forEach((element) {
     if (element % 2 == 0) {
-      print('${element},');
+      ans = k == 0 ? '${element}' : ans + ',${element}';
+      k += 1;
     }
   });
+  print(ans);
 }
