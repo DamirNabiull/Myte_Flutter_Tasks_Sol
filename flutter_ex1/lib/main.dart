3ee5b4558bff074d02c5d74e0b6e092c8071c6e9
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2021-08-21 21:37:18 +0300

    Updated solution (Flutter 1)

--- flutter_ex1/lib/main.dart
+++ flutter_ex1/lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+Widget myRow(String containerText) {
+  return FractionallySizedBox(
+    widthFactor: 1.0,
+    child: Column(
+      children: [
+        Container(
+          height: 50,
+          color: Colors.white,
+          child: Text('$containerText'),
+        ),
+        Divider(),
+      ],
+    ),
+  );
+}
+
 void main() => runApp(MaterialApp(
   home: MyHomePage(),
 ));
@@ -11,18 +27,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,21 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: ListView.builder(
         itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
-          return Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    height: 40,
-                    width: 200,
-                    child: Text('$index'),
-                    color: Colors.blue,
-                  ),
-                  Divider(),
-                ],
-              ),
-            ],
-          );
+          return myRow('${index}');
         },
       ),
     );
